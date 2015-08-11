9f49e3cff743db357df252e608e073817f98fb3d
Denis Hurtado Prada <denis_hp5@hotmail.com>
2015-08-11 15:31:47 -0600

    Cambio a Linq clase Avion
    Cambio a Linq clase Avion

--- Proyecto 1 Pr3/Proyecto1/AccesoDatos/AvionDaImpl.cs
+++ Proyecto 1 Pr3/Proyecto1/AccesoDatos/AvionDaImpl.cs
@@ -6,11 +6,9 @@
 //----Encargado: -Dennis Hurtado Prada
 //---- Llave cambio = *creaavion
 
-//----Fecha creación: 28-06-2014
-//----Descripción: Inclusion de nuevo método, listarAviones para 
-//----combobox en vuelo
-//----Encargado: -Jenniffer Chinchilla Porras
-//----Llave cambio = *listarAviones
+//----Fecha creación: 11-07-2014
+//----Descripción: cambio a linq to sql
+//----Encargado: -Denis Hurtado Prada
 /*------------------------------------------------------------
 ---------------- HISTORIAL DE MODIFICACION -----------------
 ------------------------------------------------------------*/
@@ -23,6 +21,8 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using LogicaNegocios;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace AccesoDatos
 {
@@ -31,72 +31,114 @@ namespace AccesoDatos
         //*creaavion
         public Avion buscarAvionPorId(int id)
         {
-            Avion av = null;
+            Avion av = new Avion();
             MyConnection myConnection = new MyConnection();
-            SqlConnection conexion = myConnection.CreateConnection();
-            SqlCommand comando = myConnection.CreateCommand(conexion);
-            SqlDataReader al;
+            DataContext datacontext = new DataContext(myConnection.SQLConnection);
+            var table = datacontext.GetTable<Avion>();
 
-            comando.CommandText = "sp_searchByIdPlane";
-            comando.CommandType = CommandType.StoredProcedure;
-            comando.Parameters.AddWithValue("@pId ", id);
+
             try
             {
-                conexion.Open();
-                al = comando.ExecuteReader();
-                while (al.Read())
+                var listaAvion = from lt in table
+                                 where lt.IdAvion == id
+                                 select lt;
+                foreach (Avion b in listaAvion)
                 {
-                    av = new Avion();
-                    av.IdAvion = (int)al["idAvion"];
-                    av.Marca = (string)al["marca"];
-                    av.Asientos = (int)al["cantidadAsientos"];
+                    av = b;
                 }
+
+
+
+                /*SqlConnection conexion = myConnection.CreateConnection();
+                SqlCommand comando = myConnection.CreateCommand(conexion);
+                SqlDataReader al;
+
+                comando.CommandText = "sp_searchByIdPlane";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@pId ", id);
+                try
+                {
+                    conexion.Open();
+                    al = comando.ExecuteReader();
+                    while (al.Read())
+                    {
+                        av = new Avion();
+                        av.IdAvion = (int)al["idAvion"];
+                        av.Marca = (string)al["marca"];
+                        av.Asientos = (int)al["cantidadAsientos"];
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                return av;*/
             }
+
             catch (SqlException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            finally
-            {
-                conexion.Close();
-            }
+
             return av;
+
         }
         //*listarAviones
         public List<Avion> listarAviones()
         {
             List<Avion> listaavion = new List<Avion>();
             MyConnection myConnection = new MyConnection();
-            SqlConnection conexion = myConnection.CreateConnection();
-            SqlCommand comando = myConnection.CreateCommand(conexion);
-            SqlDataReader pl;
+            DataContext datacontext = new DataContext(myConnection.SQLConnection);
+            var table = datacontext.GetTable<Avion>();
 
-            comando.CommandText = "sp_listPlane";
-            comando.CommandType = CommandType.StoredProcedure;
             try
             {
-                conexion.Open();
-                pl = comando.ExecuteReader();
-                while (pl.Read())
+                var plane = from lt in table
+                            select lt;
+                foreach (Avion b in plane)
                 {
-                    Avion creaavion = new Avion();
-                    creaavion.IdAvion = (int)pl["idAvion"];
-                    creaavion.Marca = (string)pl["marca"];
-                    creaavion.Asientos = (int)pl["cantidadAsientos"];
-
-                    listaavion.Add(creaavion);
-
+                    listaavion.Add(b);
                 }
+                // SqlConnection conexion = myConnection.CreateConnection();
+                // SqlCommand comando = myConnection.CreateCommand(conexion);
+                // SqlDataReader pl;
+
+                /* comando.CommandText = "sp_listPlane";
+                 comando.CommandType = CommandType.StoredProcedure;
+                 try
+                 {
+                     conexion.Open();
+                     pl = comando.ExecuteReader();
+                     while (pl.Read())
+                     {
+                         Avion creaavion = new Avion();
+                         creaavion.IdAvion = (int)pl["idAvion"];
+                         creaavion.Marca = (string)pl["marca"];
+                         creaavion.Asientos = (int)pl["cantidadAsientos"];
+
+                         listaavion.Add(creaavion);
+
+                     }
+                 }
+                 catch (SqlException ex)
+                 {
+                     Console.WriteLine(ex.Message);
+                 }
+                 finally
+                 {
+                     conexion.Close();
+                 }
+                return listaavion;*/
             }
             catch (SqlException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            finally
-            {
-                conexion.Close();
-            }
             return listaavion;
         }
     }
-}
+} 
