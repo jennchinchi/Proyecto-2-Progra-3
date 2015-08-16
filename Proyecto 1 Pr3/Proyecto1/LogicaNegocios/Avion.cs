fd06ca0ee4ad7ec2b3b37109087bbfb0c08c1fa6
jennchinchi <jechipo@hotmail.com>
2015-08-16 17:39:26 -0600

    Cambio de dennis
    Cambio de dennis q ronald revirtio

--- Proyecto 1 Pr3/Proyecto1/LogicaNegocios/Avion.cs
+++ Proyecto 1 Pr3/Proyecto1/LogicaNegocios/Avion.cs
@@ -4,6 +4,10 @@
 //----Fecha creación: 14-06-2015 / 01-07-2015
 //----Descripción: Creación estructura módulo avion
 //----Encargado: -Dennis Hurtado Prada
+
+//----Fecha modificacion: 11-07-2015 / 01-07-2015
+//----Descripción: linq to sql
+//----Encargado: -Dennis Hurtado Prada
 /*------------------------------------------------------------
 ---------------- HISTORIAL DE MODIFICACION -----------------
 ------------------------------------------------------------*/
@@ -12,25 +16,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
 
 namespace LogicaNegocios
 {
+    [Table(Name = "Plane")]
     public class Avion
     {
         private int idAvion;
         private int asientos;
         private String marca;
+
+        [Column(Name = "idPlane", IsPrimaryKey = true, DbType = "int NOT NULL", IsDbGenerated = true)]
         public int IdAvion
         {
             get { return idAvion; }
             set { idAvion = value; }
         }
+
+        [Column(Name = "qtyAsiento")]
         public int Asientos
         {
             get { return asientos; }
             set { asientos = value; }
         }
 
+        [Column(Name = "marca")]
         public String Marca
         {
             get { return marca; }
