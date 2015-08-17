f5a56e1ae56d10519dfb040535bb3308336a21c0
kewemst <kewemst76@gmail.com>
2015-08-17 21:58:47 +0200

    cambios kevin
    se implementa a linq las clases lugar y lugarDalmpl

--- Proyecto 1 Pr3/Proyecto1/AccesoDatos/LugarDaImpl.cs
+++ Proyecto 1 Pr3/Proyecto1/AccesoDatos/LugarDaImpl.cs
@@ -6,10 +6,16 @@
 //----Encargado: -Jenniffer Chinchilla Porras
 //----Llave cambio = *crealugar
 
-//----Fecha creación: 28-06-2014
+//----Fecha creación: 28-06-2015
 //----Descripción: Inclusion de nuevo método, listarLugares para 
 //----combobox en vuelo
 //----Encargado: -Jenniffer Chinchilla Porras
+//----Llave cambio = *combovuelo
+//
+//----Fecha modificacion: 16-08-2015
+//----Descripción: modficacion de los métodos a linq de listarLugares para 
+//----combobox en vuelo y busqueda de lugar por id 
+//----Encargado: kevin serrano 
 //----Llave cambio = *combovuelo
 /*------------------------------------------------------------
 ---------------- HISTORIAL DE MODIFICACION -----------------
@@ -23,6 +29,8 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using LogicaNegocios;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace AccesoDatos
 {
@@ -31,74 +39,95 @@ namespace AccesoDatos
         //*crealugar
         public Lugar buscarLugarPorId(int id)
         {
-            Lugar lu = null;
-            MyConnection myConnection = new MyConnection();
-            SqlConnection conexion = myConnection.CreateConnection();
-            SqlCommand comando = myConnection.CreateCommand(conexion);
-            SqlDataReader al;
+            Lugar lu = new Lugar(); // se crea la instancia de la lista que va a contener la tabla lugar
+            MyConnection myConnection = new MyConnection();//instancia de la conexion
+            DataContext datacontext = new DataContext(myConnection.SQLConnection)// instancia de datacontex y se pasa la conexion del servidor
+            var table = datacontext.GetTable<Lugar>();// se asigna la conexion a una variable la cual contiene toda la tabla
 
-            comando.CommandText = "sp_searchByIdPlace";
-            comando.CommandType = CommandType.StoredProcedure;
-            comando.Parameters.AddWithValue("@pId ", id);
+            //MyConnection myConnection = new MyConnection();
+            //SqlConnection conexion = myConnection.CreateConnection();
+            //SqlCommand comando = myConnection.CreateCommand(conexion);
+            //SqlDataReader al;
+
+            //comando.CommandText = "sp_searchByIdPlace";
+            //comando.CommandType = CommandType.StoredProcedure;
+            //comando.Parameters.AddWithValue("@pId ", id);
             try
             {
-                conexion.Open();
-                al = comando.ExecuteReader();
-                while (al.Read())
+                var listPlace = from lt in table // se crea una variable y se le asigna la consulta
+                                  where lt.IdLugar == id// se hace la comparacion del valor que se esta consultando
+                                  select lt;
+
+                foreach (Lugar b in listPlace)// se recorre la variable y se iguala el valor de la consulta
                 {
-                    lu = new Lugar();
-                    lu.IdLugar = (int)al["idLugar"];
-                    lu.Nombre = (string)al["nombre"];
-                    lu.Latitud = (string)al["latitud"];
-                    lu.Longitud = (string)al["longitud"];
+                    lu = b;
                 }
+                //conexion.Open();
+                //al = comando.ExecuteReader();
+                //while (al.Read())
+                //{
+                //    lu = new Lugar();
+                //    lu.IdLugar = (int)al["idLugar"];
+                //    lu.Nombre = (string)al["nombre"];
+                //    lu.Latitud = (string)al["latitud"];
+                //    lu.Longitud = (string)al["longitud"];
+                //}
             }
             catch (SqlException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            finally
-            {
-                conexion.Close();
-            }
-            return lu;
+            //finally
+            //{
+            //    conexion.Close();
+            //}
+            return lu;// muestra el valor consultado en la variable lu
         }
         //*combovuelo
         public List<Lugar> listarLugares()
         {
-            List<Lugar> listaLugar = new List<Lugar>();
-            MyConnection myConnection = new MyConnection();
-            SqlConnection conexion = myConnection.CreateConnection();
-            SqlCommand comando = myConnection.CreateCommand(conexion);
-            SqlDataReader pl;
+            List<Lugar> listaLugar = new List<Lugar>(); // se crea la instancia de la lista que va a contener la tabla lugar
+            MyConnection myConnection = new MyConnection();//instancia de la conexion
+            DataContext datacontext = new DataContext(myConnection.SQLConnection);// instancia de datacontex y se pasa la conexion del servidor
+            var table = datacontext.GetTable<Lugar>();// se asigna la conexion a una variable la cual contiene toda la tabla
+            //MyConnection myConnection = new MyConnection();
+            //SqlConnection conexion = myConnection.CreateConnection();
+            //SqlCommand comando = myConnection.CreateCommand(conexion);
+            //SqlDataReader pl;
 
-            comando.CommandText = "sp_listPlace";
-            comando.CommandType = CommandType.StoredProcedure;
+            //comando.CommandText = "sp_listPlace";
+            //comando.CommandType = CommandType.StoredProcedure;
             try
             {
-                conexion.Open();
-                pl = comando.ExecuteReader();
-                while (pl.Read())
+                var listPlace = from lt in table// se crea una variable y se le asigna la consulta
+                                  select lt;
+                foreach (Lugar creaLugar in listPlace)// se recorre la variable y se le agrega el valor de la consulta
                 {
-                    Lugar creaLugar = new Lugar();
-                    creaLugar.IdLugar = (int)pl["idLugar"];
-                    creaLugar.Nombre = (string)pl["nombre"];
-                    creaLugar.Latitud = (string)pl["latitud"];
-                    creaLugar.Longitud = (string)pl["longitud"];
-
                     listaLugar.Add(creaLugar);
-
                 }
+                //conexion.Open();
+                //pl = comando.ExecuteReader();
+                //while (pl.Read())
+                //{
+                //    Lugar creaLugar = new Lugar();
+                //    creaLugar.IdLugar = (int)pl["idLugar"];
+                //    creaLugar.Nombre = (string)pl["nombre"];
+                //    creaLugar.Latitud = (string)pl["latitud"];
+                //    creaLugar.Longitud = (string)pl["longitud"];
+
+                //    listaLugar.Add(creaLugar);
+
+                //}
             }
             catch (SqlException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            finally
-            {
-                conexion.Close();
-            }
-            return listaLugar;
+            //finally
+            //{
+            //    conexion.Close();
+            //}
+            return listaLugar;//muestra el valor consultado en la variable listaLugar
         }
     }
 }
--- Proyecto 1 Pr3/Proyecto1/LogicaNegocios/Lugar.cs
+++ Proyecto 1 Pr3/Proyecto1/LogicaNegocios/Lugar.cs
@@ -4,6 +4,11 @@
 //----Fecha creación: 14-06-2015 / 01-07-2015
 //----Descripción: Creación estructura módulo lugar
 //----Encargado: -Jenniffer Chinchilla Porras
+//
+//----Fecha: 14-08-2015
+//----Descripción: Se incluyen cambios en clase para hacer consultas con linq
+//----Encargado: kevin serrano arroyo
+//----
 /*------------------------------------------------------------
 ---------------- HISTORIAL DE MODIFICACION -----------------
 ------------------------------------------------------------*/
@@ -12,33 +17,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+////LINQtoSQL lugar
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
+
+//properties de la clase 
 namespace LogicaNegocios
 {
+    ////LINQtoSQL lugar
+    [Table(Name = "Place")]
     public class Lugar
     {
+        #region Variables
         private int idLugar;
         private string nombre;
         private string latitud;
         private string longitud;
-
+        #endregion
+        ////LINQtoSQL lugar
+         [Column(Name = "Longitud")]
         public string Longitud
         {
             get { return longitud; }
             set { longitud = value; }
         }
+         ////LINQtoSQL lugar
+         [Column(Name = "Latitud")]
         public string Latitud
         {
             get { return latitud; }
             set { latitud = value; }
         }
-
+         ////LINQtoSQL lugar
+         [Column(Name = "Nombre")]
         public string Nombre
         {
             get { return nombre; }
             set { nombre = value; }
         }
-       
+         ////LINQtoSQL lugar
+        [Column(Name = "IdLugar", IsPrimaryKey = true, DbType = "int NOT NULL", IsDbGenerated = true)]
         public int IdLugar
         {
             get { return idLugar; }
