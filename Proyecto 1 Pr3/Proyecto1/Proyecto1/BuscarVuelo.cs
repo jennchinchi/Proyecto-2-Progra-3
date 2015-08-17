01489653d864c923fdcfd549a8e67db933f07272
fitar3 <ronald-r3@hotmail.com>
2015-08-17 14:21:51 -0600

    Cambios en las Clases: VueloDaImpl, AgregarVuelo, BuscarVuelo y el Query
    Todo va comentado

--- Proyecto 1 Pr3/Proyecto1/Proyecto1/BuscarVuelo.cs
+++ Proyecto 1 Pr3/Proyecto1/Proyecto1/BuscarVuelo.cs
@@ -11,8 +11,8 @@
 //----Encargado: Jenniffer Chinchilla Porras
 //----Llave cambio =*capadatos
 
-//----Fecha creación: 15-08-2015
-//----Descripción: Agregar boton de cancelar 
+//----Fecha creación: 15-08-2015 // 16-08-2015
+//----Descripción: Agregar boton de cancelar // Texto en inglés
 //----Encargado: Ronald Moreira Artavia
 //----Llave cambio = camb_Bt_Cancel
 
@@ -233,8 +233,8 @@ namespace Presentacion
         {
             try
             {
-                DialogResult respuesta = MessageBox.Show("Confirmación de salida.",
-                                                         "Confirmación",
+                DialogResult respuesta = MessageBox.Show("Are you sure?.",
+                                                         "Confirmation",
                                                          MessageBoxButtons.YesNo,
                                                          MessageBoxIcon.Question);
 
@@ -246,7 +246,7 @@ namespace Presentacion
 
             catch
             {
-                MessageBox.Show("ERROR: Hay un problema al cerrar pantalla, contacte al administrador.",
+                MessageBox.Show("ERROR: There is a problem closing screen, contact your administrator.",
                                 "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             } // Fin catch
 
